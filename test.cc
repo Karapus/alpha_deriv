f14cf72eba01414c6ee2eb1cfb8aaf189fffe095
Karapus <evgeny.nay@outlook.com>
2021-05-08 22:50:38 +0300

    lex iterator

--- test.cc
+++ test.cc
@@ -9,8 +9,12 @@ TEST(lex, lexems) {
 	std::string_view s = "name +bigname /42 * 3";
 	auto b = s.begin();
 	auto e = s.end();
-	std::vector v {lex::LexemIterator{b, e}, lex::LexemIterator{e}};
+	auto lb = lex::LexemIterator{b, e};
+	auto le = lex::LexemIterator{e};
+	std::vector v(lb, le);
 	EXPECT_EQ(std::get<lex::Num>(v[4]).val, 42);
 	EXPECT_EQ(std::get<lex::Num>(v[6]).val, 3);
+	EXPECT_EQ(std::get<lex::Var>(v[0]).name, "name");
+	EXPECT_EQ(std::get<lex::Var>(v[2]).name, "bigname");
 }
 }
